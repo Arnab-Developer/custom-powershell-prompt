ae84f1da5ef12951d8f5c1c4c5823c2b55c00404
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2020-12-13 07:21:36 +0530

    Use speces instead of tabs.

--- CustomPowerShellPrompt.ps1
+++ CustomPowerShellPrompt.ps1
@@ -2,13 +2,13 @@
 # This function has been called automatically by PowerShell.
 function prompt()
 {
-	$customPrompt = `
-		[environment]::newline + "" + `
-		$env:computername + " | " + `
-		$env:username + " | " + `
-		$(get-date -format "dd MMMM, dddd hh:mm tt") + " | " + `
-		$executioncontext.sessionstate.path.currentlocation + "" + `
-		[environment]::newline + "" + "> "
+    $customPrompt = `
+        [environment]::newline + "" + `
+        $env:computername + " | " + `
+        $env:username + " | " + `
+        $(get-date -format "dd MMMM, dddd hh:mm tt") + " | " + `
+        $executioncontext.sessionstate.path.currentlocation + "" + `
+        [environment]::newline + "" + "> "
 	
-	$customPrompt
+    $customPrompt
 }
