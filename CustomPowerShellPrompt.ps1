ff39a067e5968ca48c884d735468e955e7234f03
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2020-12-15 20:35:20 +0530

    Use posh-git.

--- CustomPowerShellPrompt.ps1
+++ CustomPowerShellPrompt.ps1
@@ -1,15 +1,11 @@
 # Add this code to PowerShell Core profile.
-# This function has been called automatically by PowerShell.
-function prompt()
-{
-    $customPrompt = `
-        [environment]::newline + "" + `
-        $env:computername + " | " + `
-        $env:username + " | " + `
-        $(get-date -format "dd MMMM, dddd hh:mm tt") + " | " + `
-        $executioncontext.sessionstate.path.currentlocation + " " + `
-        $(git branch --show-current) + "" + `
-        [environment]::newline + "" + "> "
+Import-Module posh-git
 	
-    $customPrompt
-}
+$customPrompt = `
+    [environment]::newline + "" + `
+	$env:computername + " | " + `
+    $env:username + " | " + `
+    $(get-date -format "dd MMMM, dddd hh:mm tt") + " |"
+
+$GitPromptSettings.DefaultPromptPrefix.Text = '$customPrompt '
+$GitPromptSettings.DefaultPromptBeforeSuffix.Text = '`n'
